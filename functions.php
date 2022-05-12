a709037909b8e3bc5f25b92a528d24e9bae90a2a
Anton Peetso <lpstadus@gmail.com>
2022-05-12 20:56:25 +0200

    updated so the css doesn't get loaded into wp-admin

--- functions.php
+++ functions.php
@@ -1,11 +1,15 @@
 <?php
-require_once 'inc/setup.php';
+require_once "inc/setup.php";
 
 function theme_init() {
-    add_theme_support( 'align-wide' );
-    wp_enqueue_style( 'style', get_template_directory_uri() . '/public/css/main.css', array(), '1.1', 'all' );
-    wp_enqueue_script( 'script', get_template_directory_uri() . '/public/js/main.js', array ( 'jquery' ), 1.1, true);
+    add_theme_support( "align-wide" );
 }
 
-add_action('init', 'theme_init');
+add_action("init", "theme_init");
 
+function theme_enqueue_scripts() {
+    $version_number = "1.0.0";
+    wp_enqueue_style( "style", get_template_directory_uri() . "/public/css/main.css", array(), $version_number, "all" );
+    wp_enqueue_script( "script", get_template_directory_uri() . "/public/js/main.js", array ( "jquery" ), $version_number, true);
+}
+add_action( "wp_enqueue_scripts", "theme_enqueue_scripts" );
